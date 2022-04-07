a697a32c9ea474d504ad0f82b374bf5053b7d63c
abdizamedmo <abdizamedmo@gmail.com>
2022-04-07 09:57:46 +0300

    Client Phone number validated

--- src/pages/forms/clientsForm/components/Validations.js
+++ src/pages/forms/clientsForm/components/Validations.js
@@ -4,5 +4,11 @@ export const Validations = yup.object().shape({
   name: yup.string().required('Full Name is required'),
   email: yup.string().email('Invalid email').required('Email Address is required'),
   state: yup.string().required('State is required'),
-  phone: yup.string().required('Phone Number is required')
+  phone: yup
+    .number()
+    .typeError("That doesn't look like a phone number")
+    .positive("A phone number can't start with a minus")
+    .integer("A phone number can't include a decimal point")
+    .min(8)
+    .required('Phone Number is required')
 });
