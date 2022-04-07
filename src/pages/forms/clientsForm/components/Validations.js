55133fbeb3247f0d2605db5bdb4b2ef00e1bd5cf
abdizamedmo <abdizamedmo@gmail.com>
2022-04-07 09:50:16 +0300

    Client Email Required

--- src/pages/forms/clientsForm/components/Validations.js
+++ src/pages/forms/clientsForm/components/Validations.js
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 export const Validations = yup.object().shape({
   name: yup.string().required('Full Name is required'),
-  email: yup.string().required('Email Address is required'),
+  email: yup.string().email('Invalid email').required('Email Address is required'),
   state: yup.string().required('State is required'),
   phone: yup.string().required('Phone Number is required')
 });
